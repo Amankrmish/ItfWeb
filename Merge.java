79be942523dd895bf784d10de9cdbeb8db3303bb
Aman Kumar <krishnaman95@gmail.com>
2023-02-05 13:38:31 +0530

    initial commit

---
+++ Merge.java
@@ -0,0 +1,28 @@
+package com.Array;
+
+import java.util.*;
+public class Merge {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println("Enter the size of A array ");
+		int n=sc.nextInt();
+		System.out.println("Enter the size of B array");
+		int m=sc.nextInt();
+		
+		int a[]=new int[n];
+		
+		int b[]=new int[m];
+		
+		int z[]=new int[n+m];
+		
+		for(int i=0;i<a.length;i++)
+		{
+			a[i]=sc.nextInt();
+		}
+		System.out.println(a);
+
+	}
+
+}
